59c6c30d09ac20a4f3e354c282b8041dc47b05cf
alanleal-econ <prof@alanleal-econ.com>
2024-08-16 17:37:17 -0300

    Update export objects

--- src/SpatialEconometrics.jl
+++ src/SpatialEconometrics.jl
@@ -2,14 +2,23 @@ module SpatialEconometrics
 
 greet() = print("Hello World!")
 include("sar.jl")
+export sar_estimacao
 include("sem.jl")
+export sem_estimacao
 include("sarar.jl")
+export sarar_estimacao
 include("sar_summary.jl")
+export summary_sar
 include("sem_summary.jl")
+export summary_sem
 include("sarar_summary.jl")
+export summary_sarar
 include("effects_sarar.jl")
+export efeitos_sarar
 include("effects_sar.jl")
+export efeitos
 include("effects_summary.jl")
+export effects_summary
 
 
 end # module SpatialEconometrics
