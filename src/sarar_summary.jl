39fb95df7cf07db3a39ea5afed210f9cd162a3fb
alanleal-econ <prof@alanleal-econ.com>
2024-08-17 08:59:32 -0300

    Update

--- src/sarar_summary.jl
+++ src/sarar_summary.jl
@@ -16,6 +16,6 @@ function sarar_summary(sarmodel,names_col)
     df_definitive=hcat(names_columns,sarmodel.coefs)
     header = ["Variable","β", "Std Dev", "Lower CI", "Upper CI", "p-value"]
     pretty_table(df_definitive, header=header, alignment = :c, formatters = ft_printf("%.3f", 1:5),tf=tf_ascii_rounded)
-    print("λ: ", round(sarmodel.rho[1],digits=3), ", Standard Deviation: ", round(sarmodel.rho[2],digits=3), "\n")
+    print("ρ: ", round(sarmodel.rho[1],digits=3), ", Standard Deviation: ", round(sarmodel.rho[2],digits=3), "\n")
     print("λ: ", round(sarmodel.lambda[1],digits=3), ", Standard Deviation: ", round(sarmodel.lambda[2],digits=3), "\n")
 end
